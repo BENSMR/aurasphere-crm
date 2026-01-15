451bacadd22ff0e9fade5358c9883f69257466bc
bensmir <bensmir18@gmail.com>
2026-01-15 10:05:28 +0100

    fix: add Netlify _redirects for SPA routing

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b4c54d8be7eaf3e9c053954490f2ac98
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "3b6ae068f7c3cfbea7e0674f0ea23895",
+"flutter_bootstrap.js": "e934e3034b8c1b99812dbbcbdd2524ac",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
